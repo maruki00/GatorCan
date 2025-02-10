52b1e1c25a53aeaf86252caea807f86f09c454d7
Navnit <thecastercat@gmail.com>
2025-02-09 21:51:23 -0500

    initial commit

--- gatorcan-frontend/src/App.jsx
+++ gatorcan-frontend/src/App.jsx
@@ -1,48 +1,10 @@
-import { Routes, Route } from "react-router-dom";
-
 import Login from "./components/Login";
-import ProtectedRoute from "./components/ProtectedRoute";
-import ProtectedDashboard from "./components/ProtectedDashboard";
-import AdminDashboard from "./components/AdminDashboard";
-import StudentDashboard from "./components/StudentDashboard";
-import InstructorDashboard from "./components/InstructorDashboard";
 
 import "./App.css";
 
 function App() {
   return (
-    <Routes>
-      <Route path="login" element={<Login />} />
-      <Route path="dashboard" element={<ProtectedRoute />} />
-
-      {/* Protecting dashboard routes */}
-      <Route
-        path="admin-dashboard"
-        element={
-          <ProtectedDashboard allowedRoles={["admin"]}>
-            <AdminDashboard />
-          </ProtectedDashboard>
-        }
-      />
-      <Route
-        path="student-dashboard"
-        element={
-          <ProtectedDashboard allowedRoles={["student"]}>
-            <StudentDashboard />
-          </ProtectedDashboard>
-        }
-      />
-      <Route
-        path="instructor-dashboard"
-        element={
-          <ProtectedDashboard allowedRoles={["instructor"]}>
-            <InstructorDashboard />
-          </ProtectedDashboard>
-        }
-      />
-
-      <Route path="*" element={<Login />} />
-    </Routes>
+    <Login></Login>
   );
 }
 
